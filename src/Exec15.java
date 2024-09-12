94facd91289b07c36d73793cff8afecbc5e23f8d
TheGzuckert <games.zarck@gmail.com>
2024-09-12 16:05:42 -0300

    fix: correction exec 15

--- src/Exec15.java
+++ src/Exec15.java
@@ -2,30 +2,40 @@ import java.util.*;
 
 public class Exec15 {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Insira seu ano de nascimento: ");
         int anoNascimento = scan.nextInt();
 
-        int anoAtual = 2024; // ano em que estamos
-        int umAno = 365; // quantidade de dias
-        int umMes = 30; // dias
+        System.out.println("Insira o mes de nascimento: ");
+        int mesNascimento = scan.nextInt();
 
-        int idadeAtual = anoAtual - anoNascimento;
+        System.out.println("Dia de nascimento: ");
+        int diaNascimento = scan.nextInt();
 
-        int idadeEmMeses = idadeAtual;
+        int anoAtual = 2024;
+        int mesAtual = 9;
+        int diaAtual = 12;
 
-        System.out.println("Voce tem cerca de " + idadeAtual + " anos.");
-
-
-
-
-
-
+        int anosVividos = anoAtual - anoNascimento;
+        int mesesVividos = mesAtual - mesNascimento;
+        int diasVividos = diaAtual - diaNascimento;
 
 
+        if (diasVividos < 0) {
+            diasVividos += 30;
+            mesesVividos--;
+        }
 
+        if (mesesVividos < 0){
+            mesesVividos +=12;
+            anosVividos--;
+        }
 
+        int totalDiasVividos = anosVividos * 365 + mesesVividos * 30 + diasVividos;
 
+        System.out.println("Você viveu " + anosVividos + " anos, " + mesesVividos + " meses e " + diasVividos + " dias.");
+        System.out.println("Total de dias vividos: " + totalDiasVividos);
 
     }
-    }
+}
